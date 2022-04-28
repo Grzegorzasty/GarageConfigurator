0ab92715d4d3c5a16fb486f68fd23b22d0a07dd7
Grzegorz <grzeg.pasionek@gmail.com>
2022-04-28 18:22:49 +0200

    buttons and navbar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
