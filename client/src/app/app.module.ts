aaa107318769bba6ed4539b47d0b47402e7e1554
esiu <grzegorzzelek10@gmail.com>
2022-05-02 17:58:27 +0200

    refactor products component, add footer, add contact tab, clean up package.json

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,22 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule} from '@angular/router';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './configurator/contact.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    ProductsComponent
+    ProductsComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTooltipModule,
+    RouterModule.forRoot([
+      {path: 'kontakt', component: ContactComponent},
+      {path: 'oferta', component: ProductsComponent}
+
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
